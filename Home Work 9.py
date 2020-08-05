396f7c278c44c61c091dad52acfcb04c93a95631
Vitaliy-Koziak <kozyakvit@gmail.com>
2020-08-05 23:56:44 +0300

    Task done

--- Home Work 9.py
+++ Home Work 9.py
@@ -1,1 +1,33 @@
+"""У змінній записаний текст. Словом вважається послідовність непорожніх символів, які йдуть
+підряд, слова розділені одним або більше пробілом.
+
+Програмно створіть словник, в якому ключами є слова з речення, а значеннями – кількість
+входження в речення."""
+
+str = input("Enter string:  ")
+
+symbols = ['.',',','!','?',';',':',')','(']
+for symbol in symbols:
+    if symbol in str:
+        str = str.replace(symbol,'')
+
+str = str.lower()
+str_list = str.split()
+
+dictionary = {}
+
+for item in str_list:
+    dictionary[item] = str_list.count(item)
+
+for key,value in dictionary.items():
+    print(key," : ",value)
+
+
+
+
+
+
+
+
+
 
